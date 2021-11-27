523bc430502e0acbfa0fa4dbfe506b8fe4346842
Derrick Nguyen <ptkn2001@msn.com>
2021-11-27 08:32:29 -0800

    added some css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,12 +4,16 @@ const Header = () => {
 
 
     return (
-        <div>
-            <div><h1>Header</h1></div>
-            <div>
-                <p>John@gmail.com</p>
-            </div>
+        <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
+        <div className="container flex-row justify-space-between align-center text-center">
+          <h1 style={{ fontSize: '3rem' }}>
+            Expense Management System
+          </h1>
+          <p style={{ margin: '0 1rem 0 0' }}>
+            John@gmail.com
+          </p>
         </div>
+      </header>
     );
   };
   
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -18,21 +18,21 @@ const Navbar = (props) => {
         <div>
              {(props.loggedIn) ? (
                     <ul>
-                        <li className='navItem' onClick={handleClick}>Dashboard</li>
-                        <li className='navItem' onClick={handleClick}>Budget</li>
-                        <li className='navItem' onClick={handleClick}>Category</li>
-                        <li className='navItem' onClick={handleClick}>Expense</li>
-                        <li className='navItem' onClick={handleClick}>Report</li>
-                        <li className='navItem' onClick={handleClick}>Extra</li>
-                        <li className='navItem' onClick={handleLogout}>Logout</li>
+                        <li className='navItem my-1' onClick={handleClick}>Dashboard</li>
+                        <li className='navItem my-1' onClick={handleClick}>Budget</li>
+                        <li className='navItem my-1' onClick={handleClick}>Category</li>
+                        <li className='navItem my-1' onClick={handleClick}>Expense</li>
+                        <li className='navItem my-1' onClick={handleClick}>Report</li>
+                        <li className='navItem my-1' onClick={handleClick}>Extra</li>
+                        <li className='navItem my-1' onClick={handleLogout}>Logout</li>
                     </ul>
                 ) : (
                         // If we are logged out, render this:
                         <div>
                           <ul>
-                            <li className='navItem' onClick={handleClick}>Login</li>
-                            <li className='navItem' onClick={handleClick}>Signup</li>
-                            <li className='navItem' onClick={handleClick}>Extra</li>
+                            <li className='navItem my-1' onClick={handleClick}>Login</li>
+                            <li className='navItem my-1' onClick={handleClick}>Signup</li>
+                            <li className='navItem my-1' onClick={handleClick}>Extra</li>
                           </ul>
                         </div>
                       )
