969cb5701baace5056dcbb27392248c233abbc1a
ebazazzadeh <ebazazzadeh@gmail.com>
2021-11-28 19:15:00 -0800

    update Expenses

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -41,7 +41,7 @@ const Main = (props) => {
         <div>
           {expenseData.loading ? (
             <div>Loading...</div>
-          ) : (<Expense expenses={expenses} />
+          ) : (<Expense expenses={expenses} categories={categories}/>
           )}
         </div>
       );
